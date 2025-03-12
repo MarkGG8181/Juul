86baab4f013fdea918397ec905baaf4d43f9ce3e
MarkGG8181 <86848614+MarkGG8181@users.noreply.github.com>
2025-03-12 17:40:25 +0200

    pom should only have one lwjgl version

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -11,7 +11,7 @@ public class Start {
     // I am tired of copying around assets every single time.
     public static void main(String[] args) {
         System.setProperty("org.lwjgl.librarypath", new File("../../natives").getAbsolutePath());
-        
+
         final String appdataDirectory = System.getenv("APPDATA");
 
         File runDir = new File(appdataDirectory != null ? appdataDirectory : System.getProperty("user.home", "."), ".minecraft/");
